f57f27f51523a555eb5559c793e60db4ad0922b5
Ryan Ott <ryan.ott@student.uva.nl>
2024-09-15 21:07:47 +0200

    ALMA test run successful

--- run_pipeline.py
+++ run_pipeline.py
@@ -3,6 +3,10 @@ Main script to run entire pipeline (from fine-tuning, compression to evaluation)
 """
 
 import argparse
+import torch
+from peft import PeftModel
+from transformers import AutoModelForCausalLM
+from transformers import LlamaTokenizer
 
 
 def main():
@@ -21,6 +25,20 @@ def main():
     if args.evaluate:
         # evaluate_model()
         pass
+    
+    # Load base model
+    model = AutoModelForCausalLM.from_pretrained("haoranxu/ALMA-7B", torch_dtype=torch.float16, device_map="auto")
+    tokenizer = LlamaTokenizer.from_pretrained("haoranxu/ALMA-7B", padding_side='left')
+
+    # Add the source setence into the prompt template
+    prompt="Translate this from German to English:\German: Ich esse gerne Fischbrötchen wenn das Wetter schön ist.\nEnglish:"
+    input_ids = tokenizer(prompt, return_tensors="pt", padding=True, max_length=40, truncation=True).input_ids.cuda()
+
+    # Translation
+    with torch.no_grad():
+        generated_ids = model.generate(input_ids=input_ids, num_beams=5, max_new_tokens=20, do_sample=True, temperature=0.6, top_p=0.9)
+    outputs = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)
+    print(outputs)
 
 
 if __name__ == "__main__":
